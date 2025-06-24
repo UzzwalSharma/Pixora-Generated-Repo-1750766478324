80198df9be20071d635f6e4656ff911ab8fc2d22
Ujjwal Sharma <165439242+UzzwalSharma@users.noreply.github.com>
2025-06-24 17:31:27 +0530

    Add App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Analytics from './pages/Analytics';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/analytics" element={<Analytics />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
